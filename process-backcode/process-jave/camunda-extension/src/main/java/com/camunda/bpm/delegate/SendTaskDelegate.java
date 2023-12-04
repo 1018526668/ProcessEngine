950363a5416c548ccf6ee087aa18cec05795629a
‘wuwei22’ <2228959878@qq.com>
2023-12-04 18:26:01 +0800

    test

---
+++ process-backcode/process-jave/camunda-extension/src/main/java/com/camunda/bpm/delegate/SendTaskDelegate.java
@@ -0,0 +1,15 @@
+package com.camunda.bpm.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SendTaskDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        execution.getProcessEngineServices()
+                .getRuntimeService()
+                .createMessageCorrelation("message")
+                .processInstanceBusinessKey("messageBusinessKey")
+                .correlate();
+    }
+}
