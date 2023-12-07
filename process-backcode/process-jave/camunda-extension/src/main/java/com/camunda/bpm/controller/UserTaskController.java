c15321218525636d09ce78236337a2425e7dc503
‘wuwei22’ <2228959878@qq.com>
2023-12-07 18:08:10 +0800

    切换默认的数据库为mysql

--- process-backcode/process-jave/camunda-extension/src/main/java/com/camunda/bpm/controller/UserTaskController.java
+++ process-backcode/process-jave/camunda-extension/src/main/java/com/camunda/bpm/controller/UserTaskController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 @RestController
 public class UserTaskController {
