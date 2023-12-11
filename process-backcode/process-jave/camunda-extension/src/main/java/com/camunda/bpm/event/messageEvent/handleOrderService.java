ea911f957d5545bec37652471a78e4b662714064
‘wuwei22’ <2228959878@qq.com>
2023-12-11 19:48:59 +0800

    businessKey的使用，rest api json数据

--- process-backcode/process-jave/camunda-extension/src/main/java/com/camunda/bpm/event/messageEvent/handleOrderService.java
+++ process-backcode/process-jave/camunda-extension/src/main/java/com/camunda/bpm/event/messageEvent/handleOrderService.java
@@ -9,14 +9,16 @@ import org.springframework.stereotype.Component;
 public class handleOrderService implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        //把业务开始时的businessKey获取到，并传递到消息事件的执行中
+        String businessKey = delegateExecution.getBusinessKey();
         System.out.println("处理订餐订单");
         String endpoint = (String) delegateExecution.getVariable("endpoint");
         RuntimeService runtimeService = delegateExecution.getProcessEngineServices().getRuntimeService();
         if ("wechat".equals(endpoint)){
 //      Message_wechatpay、   Message_alipay   是两个事件的name
-            runtimeService.startProcessInstanceByMessage("Message_wechatpay");
+            runtimeService.startProcessInstanceByMessage("Message_wechatpay",businessKey);
         } else if ("alipay".equals(endpoint)) {
-            runtimeService.startProcessInstanceByMessage("Message_alipay");
+            runtimeService.startProcessInstanceByMessage("Message_alipay",businessKey);
         }
     }
 }
